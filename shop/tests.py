8b4c9d3aae7cef48cbfee891eeab4dad3d5b03ce
Oleg Litvin <Nenya2003@gmail.com>
2021-03-31 18:28:38 +0300

    Show my project

--- shop/tests.py
+++ shop/tests.py
@@ -2,11 +2,13 @@ from django.test import TestCase
 from .models import Product, Cart
 from django.urls import reverse
 
+
 product = Product(price=-300, amount=-6)
 
 
 class ProductCheck(TestCase):
-    product.checking()
+    """Checking product accuracy"""
+    product.checking_product_accuracy()
 
     def test_validation_of_price(self):
         self.assertGreaterEqual(product.price, 0)
@@ -16,15 +18,21 @@ class ProductCheck(TestCase):
 
 
 class CartCheck(TestCase):
-
+    """Checking cart accuracy"""
     def test_cost_validation(self):
         cart = Cart(cost=-55)
-        cart.cart_check()
+        cart.checking_cart_accuracy()
         self.assertGreaterEqual(cart.cost, 0)
 
 
 class LinksTest(TestCase):
-
+    """Checking work of links"""
+    """Checking main page"""
     def test_main_page(self):
         response = self.client.get(reverse('shop:main_page'))
+        self.assertEqual(response.status_code, 200)
+
+    """Checking removing page"""
+    def test_product_in_category(self):
+        response = self.client.get(reverse('shop:remove_item'))
         self.assertEqual(response.status_code, 200)
